2d4180a082cdcd9b679a3a9c914334ba71be08e9
Jingyi Wang <jingyi.5.wang@kcl.ac.uk>
2022-10-03 22:11:23 +0100

    median

--- median.py
+++ median.py
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+
+
 
 while True:
     try:
@@ -10,3 +13,32 @@ while True:
     else:
         break
 print(numbers)
+
+numbers.sort()
+
+if (len(numbers) % 2 == 1):
+    print(numbers[len(numbers) // 2])
+else:
+=======
+"""Median calculator."""
+"""ENTER YOUR SOLUTION HERE!"""
+
+
+
+while True:
+    try:
+        print("Enter a list of numbers separated by commas: ")
+        numbers = [float(value) for value in input().split(",")]
+    except ValueError:
+        print("Some input could not be converted to a number!")
+    else:
+        break
+print(numbers)
+
+numbers.sort()
+
+if (len(numbers) % 2 == 1):
+    print(numbers[len(numbers) // 2])
+else:
+>>>>>>> 4acb48bac781421d489deb887f0d86c9cf69b5cc
+    print(((numbers[len(numbers) // 2]) + (numbers[len(numbers) // 2 - 1])) / 2)
