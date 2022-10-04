5e6e5a2dc76d1e8e9523ce2f1f7fd8b19f76b20c
wanjw270 <114835721+wanjw270@users.noreply.github.com>
2022-10-04 14:16:27 +0100

    RemovingDuplicates

--- median.py
+++ median.py
@@ -4,26 +4,6 @@
 
 
 
-while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
-
-numbers.sort()
-
-if (len(numbers) % 2 == 1):
-    print(numbers[len(numbers) // 2])
-else:
-=======
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
-
 
 while True:
     try:
