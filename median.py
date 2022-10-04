fbfae757fa2c3d5e31ca4d7a8764bfe0c758ef36
wanjw270 <114835721+wanjw270@users.noreply.github.com>
2022-10-04 14:17:08 +0100

    FinishedUpdate

--- median.py
+++ median.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
@@ -20,5 +20,5 @@ numbers.sort()
 if (len(numbers) % 2 == 1):
     print(numbers[len(numbers) // 2])
 else:
->>>>>>> 4acb48bac781421d489deb887f0d86c9cf69b5cc
+
     print(((numbers[len(numbers) // 2]) + (numbers[len(numbers) // 2 - 1])) / 2)
